112a25ee4ea8d7328afbaad8fa8b4ff36100149a
Astrid Avalin Soerensen <Avalin@users.noreply.github.com>
2020-11-27 16:56:04 +0100

    -

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.send('index.html')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
